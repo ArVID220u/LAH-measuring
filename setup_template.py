c6815bcccb4bd7b777bcbe9afb03598c69755055
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2016-06-26 18:01:29 +0200

    Updated setup

--- setup_template.py
+++ setup_template.py
@@ -1,13 +1,16 @@
 # Copy this file to a file named "setup.py", and fill in all desired values
 
 # The Twitter API keys needed to send tweets
+# Only read access is needed
 CONSUMER_KEY = "enter your consumer key here"
 CONSUMER_SECRET = "enter your secret consumer key here"
 ACCESS_TOKEN = "enter your access token here"
 ACCESS_TOKEN_SECRET = "enter your secret access token here"
 
-# The path to the folder where to put all collected data
-DATA_PATH = "~/path/to/data/folder"
+# the path to the file containing the json objects of the collected tweets
+# the raw data, essentially
+DATA_PATH = "~/path/to/raw/data.json"
 
 # The path to the file containing the user ids of the accounts to be analyzed
-USER_IDS_PATH = "~/path/to/user/ids.txt"
+# The file has to be a csv file
+USER_IDS_PATH = "~/path/to/user/ids.csv"
