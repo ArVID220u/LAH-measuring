2669dd184f813b78786b5efc09c39ce6120a946e
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2016-06-26 13:53:40 +0200

    Added data and user id options in setup

--- setup_template.py
+++ setup_template.py
@@ -1,6 +1,13 @@
-# The Twitter API keys needed to send tweets
+# Copy this file to a file named "setup.py", and fill in all desired values
 
+# The Twitter API keys needed to send tweets
 CONSUMER_KEY = "enter your consumer key here"
 CONSUMER_SECRET = "enter your secret consumer key here"
 ACCESS_TOKEN = "enter your access token here"
 ACCESS_TOKEN_SECRET = "enter your secret access token here"
+
+# The path to the folder where to put all collected data
+DATA_PATH = "~/path/to/data/folder"
+
+# The path to the file containing the user ids of the accounts to be analyzed
+USER_IDS_PATH = "~/path/to/user/ids.txt"
