b71a7670e14611a148a47b8fae31052318ca6ffc
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2016-09-04 17:26:16 +0200

    add twitter api keys to setup

--- setup_template.py
+++ setup_template.py
@@ -42,9 +42,30 @@ USER_IDS_PATH = "user_ids.txt"
 
 # API KEYS
 
-# The Twitter API keys needed to send tweets
-# Only read access is needed
-CONSUMER_KEY = "enter your consumer key here"
-CONSUMER_SECRET = "enter your secret consumer key here"
-ACCESS_TOKEN = "enter your access token here"
-ACCESS_TOKEN_SECRET = "enter your secret access token here"
+# Keys for the Tweeting bot. Needs read and write privileges.
+TWEETING_CONSUMER_KEY = "enter your consumer key here"
+TWEETING_CONSUMER_SECRET = "enter your secret consumer key here"
+TWEETING_ACCESS_TOKEN = "enter your access token here"
+TWEETING_ACCESS_TOKEN_SECRET = "enter your secret access token here"
+
+# Keys for the Mentions bot. Needs read and write privileges.
+MENTIONS_CONSUMER_KEY = "enter your consumer key here"
+MENTIONS_CONSUMER_SECRET = "enter your secret consumer key here"
+MENTIONS_ACCESS_TOKEN = "enter your access token here"
+MENTIONS_ACCESS_TOKEN_SECRET = "enter your secret access token here"
+
+# Keys for the Tweeting bot. Needs read privileges.
+MEASURING_CONSUMER_KEY = "enter your consumer key here"
+MEASURING_CONSUMER_SECRET = "enter your secret consumer key here"
+MEASURING_ACCESS_TOKEN = "enter your access token here"
+MEASURING_ACCESS_TOKEN_SECRET = "enter your secret access token here"
+
+# A string indicating the screen name of a twitter user who should receive error messages via DM (again, screen name without '@')
+# Set this to None to not send error messages to anyone, and then don't bother filling in the api keys
+# Note that the recipient must either follow the bot, or have opened their DMs to all
+ERROR_MESSAGE_RECIPIENT_SCREEN_NAME = None
+# The error message app needs read, write and DM privileges.
+ERROR_MESSAGE_CONSUMER_KEY = "if enabling error messaging, enter your error message twitter application consumer key"
+ERROR_MESSAGE_CONSUMER_SECRET = "if enabling error messaging, enter your error message twitter application consumer secret key"
+ERROR_MESSAGE_ACCESS_TOKEN = "if enabling error messaging, enter your error message twitter application access token"
+ERROR_MESSAGE_ACCESS_TOKEN_SECRET = "if enabling error messaging, enter your error message twitter application secret access token"
