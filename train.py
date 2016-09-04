9779489a1bd6e47a60b2d1b46b3463693a3445e5
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2016-09-04 17:49:53 +0200

    minor changes

--- train.py
+++ train.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 
 # Run this script for a simple way to train the sentiment analyzer
+# Stream random tweets and send them to the sentiment_database
+
+# import setup
+import setup
+
