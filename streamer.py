0a31a28d03d99d3c56b39f1f3c733be504ddc396
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2016-10-23 02:11:51 +0200

    Signed functions in streamer

--- streamer.py
+++ streamer.py
@@ -3,7 +3,7 @@
 # the TweetStreamer is a subclass of TwythonStreamer
 from twython import TwythonStreamer
 # errors!
-from error_messenger import send_error_message
+import error_messenger
 
 # the TweetStreamer class will use the streaming api to check for new tweets.
 # It will be used for filtering all tweets containing the trigger word specified in setup.py
@@ -11,21 +11,21 @@ from error_messenger import send_error_message
 class TweetStreamer(TwythonStreamer):
 
     # Simple label to know from where the tweet streamer was called
-    error_title = "placeholder"
+    arvid220u_error_title = "placeholder"
 
     # Normally, retweets should be excluded
-    exclude_retweets = True
+    arvid220u_exclude_retweets = True
 
-    new_tweet_observers = []
+    arvid220u_new_tweet_observers = []
 
-    def add_observer(self, observer):
-        self.new_tweet_observers.append(observer)
+    def arvid220u_add_observer(self, observer):
+        self.arvid220u_new_tweet_observers.append(observer)
 
     # this function will be called when a tweet is received
     def on_success(self, data):
         if "text" not in data:
             return
-        if self.exclude_retweets:
+        if self.arvid220u_exclude_retweets:
             # filter out retweets
             if data["text"].startswith("RT"):
                 return
@@ -33,7 +33,7 @@ class TweetStreamer(TwythonStreamer):
                 return
         # send tweet to the specified delegate function
         # self.new_tweet needs to be set
-        for observer in self.new_tweet_observers:
+        for observer in self.arvid220u_new_tweet_observers:
             observer(data)
 
     # when an error is caught
@@ -41,7 +41,13 @@ class TweetStreamer(TwythonStreamer):
         print("STREAMING API ERROR IN TWEETSTREAMER!")
         print("Status code:")
         print(status_code)
-        send_error_message("streaming API error, with code " + str(status_code), "TweetStreamer.on_error from " + error_title)
+        error_messenger.send_error_message("streaming API error, with code " + str(status_code), "TweetStreamer.on_error from " + arvid220u_error_title)
         print("Other data:")
         print(data)
+        print("END OF ERROR MESSAGE")
+
+    # on timeout
+    def on_timeout(self):
+        print("STREAMING API TIMEOUT IN TWEETSTREAMER!")
+        error_messenger.send_error_message("streaming API timeout", "TweetStreamer.on_timeout from " + arvid220u_error_title)
         print("END OF ERROR MESSAGE")
