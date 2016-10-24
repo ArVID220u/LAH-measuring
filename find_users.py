2a7c3a6f7f6f61cb66e7e980a8dcc7d6de78739c
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2016-10-24 20:42:49 +0200

    Score users function

--- find_users.py
+++ find_users.py
@@ -8,6 +8,13 @@ import setup
 # import threading
 from threading import Thread
 # import the sentiment analysis
+from sentiment_analyzer import SentimentAnalyzer
+from sentiment_analyzer import SentimentClassification
+# import twythonaccess
+import twythonaccess
+from twythonaccess import TwitterApp
+# import random for the simulations
+import random
 
 
 # How to do this?
@@ -28,6 +35,10 @@ user_ids = []
 
 
 def main():
+    # before doing anything else, initialize the sentiment analyzer
+    global sentiment_analyzer
+    sentiment_analyzer = SentimentAnalyzer()
+    print("initialized sentiment analyzer")
     # define two threads: one user_abort, and one setup_streamer (which also, incidentally, starts the streamer)
     abort_thread = Thread(target = user_abort)
     streamer_thread = Thread(target = setup_streamer)
@@ -85,11 +96,74 @@ def setup_streamer():
 # Disregard both kind and neutral tweets, since they are here deemed to have no impact
 def score_user(user_id):
     print("score user")
+    # 1. Create list of tweet texts
+    user_tweets = twythonaccess.authorize(TwitterApp.tweeting).get_user_timeline(user_id = user_id, count = 200, trim_user = True, include_rts = False)
+    tweet_texts = []
+    for tweet in user_tweets:
+        if "text" not in tweet:
+            continue
+        # filter out retweets
+        if tweet["text"].startswith("RT"):
+            continue
+        if "retweeted_status" in tweet:
+            continue
+        tweet_texts.append(tweet["text"])
+    # (1.1. Ensure the length of the tweet texts list is longer than 10 tweets – otherwise, treat it as statistically unreliable data, and return immediately)
+    if len(tweet_texts) < 10:
+        return
+
+    # 2. Create a list of probability distributions based on each tweet
+    probability_distributions = []
+    for tweet_text in tweet_texts:
+        probability_distributions.append(sentiment_analyzer.analyze_tweet_probability_distribution(tweet_text))
+
+    # 3. Make 10 000 simulations, and calculate the mean ratio of hateful tweets in relation to all tweets
+    mean_hatefulness_score = 0
+    num_simulations = 500
+    for i in range(1,num_simulations):
+        number_of_hateful_tweets = 0
+        for prob_distribution in probability_distributions:
+            # The sum of all probabilities
+            prob_sum = 0
+            hateful_prob = 0
+            for label in prob_distribution:
+                prob_sum += prob_distribution[label]
+                if label == SentimentClassification.Hateful:
+                    hateful_prob = prob_distribution[label]
+            # check if the classification is hateful, based on randomly generated numbers within the probability range
+            # recall that random.random() returns a random number between 0 and 1
+            if prob_sum * random.random() <= hateful_prob:
+                ++number_of_hateful_tweets
+        # add this to the hatefulness score
+        mean_hatefulness_score += number_of_hateful_tweets / len(probability_distributions)
+    # make the sum a mean
+    mean_hatefulness_score /= num_simulations
+
+    # 4. Get the minimum elemnt in user_ids, along with its index, and add this user if needed
+    # first check if length of user ids is less than the maximum number of users
+    if len(user_ids) < setup.NUMBER_OF_USERS:
+        user_ids.append((user_id, mean_hatefulness_score))
+        return
+    # get the min index
+    min_index = 0
+    min_hatefulness_score = 2
+    for index, (some_user_id, some_hatefulness_score) in enumerate(user_ids):
+        if some_hatefulness_score < min_hatefulness_score:
+            min_index = index
+            min_hatefulness_score = some_hatefulness_score
+    # check whether this hatefulness score is greater than the current
+    if mean_hatefulness_score > min_hatefulness_score:
+        # update the user_ids list
+        user_ids[min_index] = (user_id, mean_hatefulness_score)
+    # DONE
+
 
 
 def new_tweet(tweet):
     # only proceed to score the user if this tweet is classified as hateful
     print("new tweet")
+    if sentiment_analyzer.analyze_tweet_verdict(tweet["text"]) == SentimentClassification.Hateful:
+        score_user(tweet["user"]["id"])
 
 
 
