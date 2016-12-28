5da6a01d2fbeef616959aae7cc726d8075724109
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2016-12-28 01:22:12 +0100

    Finalize tweet.py

--- tweet.py
+++ tweet.py
@@ -30,10 +30,16 @@ from threading import Thread
 from datetime import datetime
 # errors!
 import error_messenger
+# for shuffling the user ids list
+import random
+# we need to be able to sleep
+import time
 
 
 # the unscrambled list of user ids, as loaded directly from the user id text file
 user_ids = []
+# this is a dictionary containing the screen name for every user id
+screen_name_for_user_id = {}
 
 # a dictionary of user_id -> sent responses, so as to not send the same response to the same user twice
 # thus, dict<int, set<string>>, as expressed in some pseudo-c++ syntax (set instead of list?)
@@ -72,6 +78,18 @@ def setup():
         for line in user_ids_file:
             user_id, hatefulness_score = [int(x) for x in line.strip().split()]
             user_ids.append(user_id)
+
+    # find the screen name for every user id
+    global screen_name_for_user_id
+    for user_id in user_ids:
+        # use the tweeting app for checking up the user
+        # if rate limited, wait for 1 minute, and then try again
+        # the show user request can be sent 900 times per 15 minute window
+        while twythonaccess.currently_rate_limited(TwitterApp.tweeting, 900):
+            time.sleep(60)
+        # get the screen name of the user
+        screen_name = twythonaccess.authorize(TwitterApp.tweeting).show_user(user_id = user_id)["screen_name"]
+        screen_name_for_user_id[user_id] = screen_name
 
     # create the dictionary of empty sets per each user id
     global sent_responses_to_user
@@ -166,8 +184,79 @@ def new_mention(tweet):
 
 # The tweet loop runs indefinitely, sending one tweet per week per user, at a randomly assigned time.
 # Needs thorough error handling.
+# If an error occurs here, a lot is lost on the experiment
+# Therefore, immediately retry a couple of times upon receiving the error
 def tweet_loop():
+    global user_ids
+    global sent_responses_to_user
+    global responses
+    global screen_name_for_user_id
     print("tweet loop")
+    # have an infinte loop
+    # every loop iteration should take one week, and in each iteration, exactly one tweet should be sent to each user
+    while True:
+        start_time = datetime.utcnow()
+        # first, scramble the user ids list so as to make the sending of the users completely random
+        user_ids_sendlist = user_ids[:]
+        random.shuffle(user_ids_sendlist)
+        # calculate the interval, so as to make the loop week-long
+        # we do care about achieving perfect week-loops, which is why we make the interval a tiny bit shorter (one hour) than actually needed, and storing the starting time
+        # (the reason we care is for measuring purposes, and credibility in statistics, etc)
+        # the tweet interval is measured in seconds
+        tweet_interval = ((7*24*60*60-60*60) / len(user_ids))
+        # now iterate over each user id in the sendlist
+        for user_id in user_ids_sendlist:
+            # randomly choose a tweet from the response list
+            # do it repeatedly until a response that has not yet been sent to this user is found
+            # first, check whether the response set for this user has a length that is equal to the response list – if so, reset it
+            if len(sent_responses_to_user[user_id]) >= len(responses):
+                sent_responses_to_user[user_id] = set()
+            response = responses[random.randint(0,len(responses)-1)]
+            while response in sent_responses_to_user[user_id]:
+                response = responses[random.randint(0,len(responses)-1)]
+            # send this response to the user, mentioning them
+            response_tweet = "@" + screen_name_for_user_id[user_id] + " " + response
+            # send this tweet
+            # don't care whether it is sent or not – as long as there are not too many users, it should be sent without any problem
+            # risk is twitter banning the bot due to its messages being considered unsolicited and rude
+            try:
+                twythonaccess.send_tweet(response_tweet, TwitterApp.tweeting)
+            except Exception as exception:
+                # oh no! an error occured
+                # well then. just sleep for sixteen minutes (we have one hour spare), and try once again. if it doesn't work this time, something's severly wrong
+                print(exception)
+                error_messenger.send_error_message(exception, "tweet.py > tweet_loop()")
+                print("will sleep for twenty minutes to try to avoid the exception")
+                time.sleep(16*60)
+                print("has slept for twenty minutes and will retry sending the tweet")
+                try:
+                    twythonaccess.send_tweet(response_tweet, TwitterApp.tweeting)
+                except Exception as exception2:
+                    # no no no no no!
+                    # this is not where we want to end up
+                    # switch to the backup tweeting app, by setting the twythonaccess backup mode to on
+                    # also send an urgency error message, explaining what's happening
+                    print(exception)
+                    print("toggling backup mode in tweeting app")
+                    twythonaccess.tweeting_in_backup_mode = not twythonaccess.tweeting_in_backup_mode
+                    error_messenger.send_error_message("IMPORTANT: Tweeting app now toggled its backup mode", "tweet.py > tweet_loop()")
+                    try:
+                        twythonaccess.send_tweet(response_tweet, TwitterApp.tweeting)
+                    except Exception as exception3:
+                        # i have no idea what to do by now. probably, just shut the whole thing down
+                        # we're doomed if we reach this point
+                        # goodbye, world
+                        print(exception)
+                        error_messenger.send_error_message(exception, "tweet.py > tweet_loop()")
+                        error_messenger.send_error_message("We're all doomed. Exception couldn't be resolved, even after tremendous effort. Now, ignoring the error.", "tweet.py > tweet_loop()")
+            # add the chosen response to the sent responses set
+            sent_responses_to_user.add(response)
+            # now, sleep for the specified interval
+            time.sleep(tweet_interval)
+        # great. all users have been addressed
+        # now, sleep until exactly one week has passed since the start time
+        while (datetime.utcnow() - start_time).total_seconds() <= 7*24*60*60:
+            time.sleep(1)
 
 
 # if called directly (as in "python3 mainbot.py"), then call main() function
