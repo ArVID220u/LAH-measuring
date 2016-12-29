e7d37f0bbbbea765b7e851b49569ca7459d2713a
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2016-12-29 16:21:22 +0100

    Measure.py first complete revision

--- measure.py
+++ measure.py
@@ -16,8 +16,14 @@ import setup
 from threading import Thread
 # import datetime
 from datetime import datetime
+from datetime import timedelta
 # errors!
 import error_messenger
+# sleep is important
+import time
+# import the sentiment analysis
+from sentiment_analyzer import SentimentAnalyzer
+from sentiment_analyzer import SentimentClassification
 
 
 # OK. We will store every incoming tweet (excluding those that are RTs and such) in the raw data json
@@ -25,6 +31,7 @@ import error_messenger
 # 0. day 1. offensiveness freq, 2. neutral freq, 3. kind freq, 4. combined score, 5. total freq
 # The processed data, then, will not be using the probability distributions –  only the most likely classification
 # Thus, it should be interpreted with caution: it is not very reliable data. It should be used as a first sign, an indication of the results, and not the results themselves
+# The combined score is calculated by assigning -1 to offensive tweets, 0 to neutral tweets, and 1 to kind tweets
 
 
 # a simple function for checking whether the specified file path is empty or not
@@ -46,16 +53,16 @@ self_destruction_flag = False
 user_ids = []
 
 
+# the data, resetted each day
+offensive_frequency = 0
+neutral_frequency = 0
+kind_frequency = 0
+combined_score = 0
+total_frequency = 0
+
+
 # The main function, taking control of everything
 def main():
-    # first check to see whether or not the data files are empty or not
-    # if they are nonempty, then notify, and quit
-    if is_non_zero_file(setup.RAW_DATA_PATH) || is_non_zero_file(setup.PROCESSED_DATA_PATH):
-        print("Data already exists, either at " + setup.RAW_DATA_PATH + " or at " + setup.PROCESSED_DATA_PATH + ".")
-        print("Delete these files (after having copied its contents, perhaps), and try again.")
-        print("Measure.py will now self destruct.")
-        import sys
-        sys.exit()
     # do some setup
     setup()
     # we will have two threads.
@@ -95,15 +102,54 @@ def tweet_streamer():
     # set up the streamer
     # use the measuring app
     global streamer_object
+    global user_ids
     streamer_object = TweetStreamer(setup.MEASURING_CONSUMER_KEY, setup.MEASURING_CONSUMER_SECRET, setup.MEASURING_ACCESS_TOKEN, setup.MEASURING_ACCESS_TOKEN_SECRET)
     # for error logs
-    mentions_streamer.arvid220u_error_title = "measure.py > tweet_streamer()"
+    streamer_object.arvid220u_error_title = "measure.py > tweet_streamer()"
     # add the observer (the new_mention method)
-    streamer.arvid220u_add_observer(new_tweet)
+    streamer_object.arvid220u_add_observer(new_tweet)
     # start streaming
     
     while not self_destruction_flag:
-        # start the filtering, with error handling
+        try:
+            # RTs will automatically be discarded (default setting)
+            # check for tweets written by any of the user ids in our follow list
+            streamer_object.statuses.filter(follow = user_ids)
+        except Exception as exception:
+            if self_destruction_flag:
+                continue
+            # oh no! an error occurred
+            # this is not good. not good at all. we don't want the measuring process to have a hole in it
+            # we want complete data.
+            # thus, try immediately with the backup twitter app
+            print("measure.py > tweet_streamer(): ")
+            print(exception)
+            error_messenger.send_error_message(exception, "measure.py > tweet_streamer()")
+            error_messenger.send_error_message("Starting backup measuring app. Not good.", "measure.py > tweet_streamer()")
+
+            try:
+                # reinitialize the streamer object
+                streamer_object = TweetStreamer(setup.MEASURING_BACKUP_CONSUMER_KEY, setup.MEASURING_BACKUP_CONSUMER_SECRET, setup.MEASURING_BACKUP_ACCESS_TOKEN, setup.MEASURINGBACKUP_ACCESS_TOKEN_SECRET)
+                # for error logs
+                streamer_object.arvid220u_error_title = "measure.py > tweet_streamer()"
+                # add the observer (the new_mention method)
+                streamer_object.arvid220u_add_observer(new_tweet)
+                # try again, same thing
+                # if it fails this time, then i don't know what to do
+                streamer_object.statuses.filter(follow = user_ids)
+            except Exception as exception:
+                if self_destruction_flag:
+                    continue
+                # well
+                # nothing to do, I guess
+                # beyond sending error messages, sleeping for an hour, and hoping for the best
+                print("measure.py > tweet_streamer(): ")
+                print(exception)
+                print("CRITICAL. BACKUP FAILED.")
+                error_messenger.send_error_message(exception, "measure.py > tweet_streamer()")
+                error_messenger.send_error_message("CRITICAL. BACKUP MEASURING BOT FAILED. Will now sleep for five minutes.", "measure.py > tweet_streamer()")
+                time.sleep(5*60)
+
 
     # now, we have finished streaming, and are probably in the self destruct phase
     # end the json by removing the last character (the comma) and subsituting it for a closed square bracket
@@ -118,16 +164,71 @@ def tweet_streamer():
 
 # this method is called once for every new tweet from the streamer
 def new_tweet(tweet):
+    global sentiment_analyzer
     print("new tweet")
+    # simply print this tweet's json string to the raw data (along with an appended comma)
+    # also analyze it for sentiment, and increment the appropriate day tallies
+    # start by writing the content to the raw file
+    with open(setup.RAW_DATA_PATH, "a") as raw_file:
+        raw_file.write(json.dumps(tweet))
+        raw_file.write(",")
+    # analyze the sentiment
+    sentiment_verdict = sentiment_analyzer.analyze_tweet_verdict(tweet["text"])
+    # increment the total tally
+    total_frequency += 1
+    # update the respective frequency
+    if sentiment_verdict == SentimentClassification.Hateful:
+        offensive_frequency += 1
+        combined_score -= 1
+    elif sentiment_verdict == SentimentClassification.Neutral:
+        neutral_frequency += 1
+    elif sentiment_verdict == SentimentClassification.Kind:
+        kind_frequency += 1
+        combined_score += 1
 
 
 
 # a 24-hour-long loop collecting the data
 def process_data_loop():
-    print("process data loop")
+    # this loop should run continuously, until the self destruct flag is set
+    # the last recorded time
+    last_time = datetime.utcnow()
+    while not self_destruction_flag:
+        # start with a sleep, until exactly 24 hours has passed since the last_time
+        next_time = last_time + timedelta(seconds = 24*60*60)
+        time.sleep((next_time - datetime.utcnow()).total_seconds())
+        if self_destruction_flag:
+            break
+        # write the current variables to the processed file
+        with open(setup.PROCESSED_DATA_PATH, "a") as processed_file:
+            processed_file.write(str(next_time) + ",")
+            processed_file.write(str(offensive_frequency) + "," + str(neutral_frequency) + "," + str(kind_frequency) + "," + str(combined_score) + "," + str(total_frequency))
+            processed_file.write("\n")
+        # reset the variables
+        offensive_frequency = 0
+        neutral_frequency = 0
+        kind_frequency = 0
+        combined_score = 0
+        total_frequency = 0
+        # last time is next time
+        last_time = next_time
+
 
 
 def setup():
+    # before doing anything else, initialize the sentiment analyzer
+    global sentiment_analyzer
+    sentiment_analyzer = SentimentAnalyzer()
+    print("initialized sentiment analyzer")
+
+    # first check to see whether or not the data files are empty or not
+    # if they are nonempty, then notify, and quit
+    if is_non_zero_file(setup.RAW_DATA_PATH) || is_non_zero_file(setup.PROCESSED_DATA_PATH):
+        print("Data already exists, either at " + setup.RAW_DATA_PATH + " or at " + setup.PROCESSED_DATA_PATH + ".")
+        print("Delete these files (after having copied its contents, perhaps), and try again.")
+        print("Measure.py will now self destruct.")
+        import sys
+        sys.exit()
     # Set up the two data files
     # the raw data file should be in json format
     # thus, write to it an square bracket
