9ecf499c97137af193855579c30f54f1bd20a227
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2017-01-01 02:56:01 +0100

    New classifications for new training data

--- sentiment_classification.py
+++ sentiment_classification.py
@@ -3,10 +3,10 @@
 
 # this simple helper class organizes classifications and makes them more type safe (although python never will be fully type safe)
 class SentimentClassification():
-    Hateful = "hateful"
-    Neutral = "neutral"
-    Kind = "kind"
+    hate_speech = "hate_speech"
+    offensive_but_not_hate_speech = "offensive_but_not_hate_speech"
+    not_offensive = "not_offensive"
     # a set of all classifications
-    classification_set = {"hateful", "neutral", "kind"}
+    classification_set = {"hate_speech", "offensive_but_not_hate_speech", "not_offensive"}
     # a list of all classifications
-    classification_list = ["hateful", "neutral", "kind"]
+    classification_list = ["hate_speech", "offensive_but_not_hate_speech", "not_offensive"]
