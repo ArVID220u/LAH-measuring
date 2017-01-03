cdd543d252b82c58904c0a72144cc7eaf3f5f2a6
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2017-01-03 19:23:59 +0000

    Shorter testing duration

--- manager.py
+++ manager.py
@@ -40,12 +40,13 @@ def main():
     # 30 days later, we have the tweet start time
     # Another 30 days later, we have the tweet end time
     # And, finally, another 30 days, and we have the (global) end time
-    # TESTING: ONLY 0.1 DAYS, INSTEAD OF 30 DAYS
+    # TESTING: ONLY 0.03 DAYS, INSTEAD OF 30 DAYS
     now_time = datetime.utcnow()
-    start_time = datetime.combine(now_time.date(), time(now_time.hour + 1, 0))
-    tweet_start_time = start_time + timedelta(days = 0.1)
-    tweet_end_time = tweet_start_time + timedelta(days = 0.1)
-    end_time = tweet_end_time + timedelta(days = 0.1)
+    #start_time = datetime.combine(now_time.date(), time(now_time.hour + 1, 0))
+    start_time = now_time # TESTING
+    tweet_start_time = start_time + timedelta(days = 0.03)
+    tweet_end_time = tweet_start_time + timedelta(days = 0.03)
+    end_time = tweet_end_time + timedelta(days = 0.03)
 
     print("INFORMATION")
     print("Will start at " + str(start_time) + " (UTC).")
